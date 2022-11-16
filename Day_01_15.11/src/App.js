faf42571e5cf810eda98710c562269119c1f7ea7
risacker <1906192@kiit.ac.in>
2022-11-17 00:23:09 +0530

    initial commit

---
+++ Day_01_15.11/src/App.js
@@ -0,0 +1,11 @@
+const heading = React.createElement(
+    "h1",
+    {
+        id : "title"
+    },
+    "Namaste React"
+);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
